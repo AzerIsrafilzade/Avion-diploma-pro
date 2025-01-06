1cf236d620b6cedec19a1e27f7dda09afea581bc
AzerIsrafilzade <azisrafilzade@gmail.com>
2025-01-07 02:45:24 +0400

    project created

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import Header1 from './components/Header1'
+import Header2 from './components/Header2'
+import Introduction from './components/Introduction';
+
+
+function App() {
+  return (
+    <>
+      <Header1 />
+      <Header2 />
+      <Introduction/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header1.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+import './Header1.css';
+import search from "../assets/images/header/Search.png";
+import basket from "../assets/images/header/Basket.png";
+import account from "../assets/images/header/Account.png";
+import { Link } from "react-router-dom";
+
+const Header1: React.FC = () => {
+  return (
+    <header >
+      <div className="header-top-container">
+        <div className="header-content">
+          <img className="header-icon" src={search} alt="Search Icon" />
+         <Link to="/" className="avion">Avion</Link> 
+          <div className="header-icon-container">
+          <img className="header-icon" src={basket} alt="basket Icon"  />
+          <img className="header-icon" src={account} alt="account Icon" />
+          </div>
+         
+
+          
+
+         
+          
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header1;
+
+
