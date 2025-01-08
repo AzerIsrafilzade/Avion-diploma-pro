e657606eb4841d110e0863e96ccd423be5ad9841
AzerIsrafilzade <azisrafilzade@gmail.com>
2025-01-09 00:35:09 +0400

    ceramiccards added

---
+++ src/components/Ceramiccards/Ceramiccards.tsx
@@ -0,0 +1,81 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "./Ceramiccards.css"
+import Ceramiccard from './Ceramiccard'
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+
+interface Melumat {
+  id:number,
+  ceramic_image:string,
+  ceramic_context:string,
+  ceramic_price:string
+}
+
+const Ceramiccards: React.FC = () => {
+
+
+  const [data, setData] = useState<Melumat[]>([]); 
+  useEffect(() => {
+    
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/ceramic.json"); 
+        setData(response.data);
+      
+      } catch (error) {
+        console.error("Data fetch error:", error);
+       
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div className='ceramiccard-container'>
+      <h3>New ceramics</h3>
+      <div className='swiper-container'>
+      <Swiper
+    slidesPerView={3}
+    spaceBetween={30}
+    autoplay={{ delay: 2500, disableOnInteraction: false }}
+    loop={true}
+
+    breakpoints={{
+        320: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 1,
+        },
+      }}
+    
+    className="mySwiper"
+  >
+
+
+        {
+          data && data.map(birMelumat => (
+            <SwiperSlide key={birMelumat.id}>
+                 <Ceramiccard imageProps ={birMelumat.ceramic_image} contextProps={birMelumat.ceramic_context} priceProps={birMelumat.ceramic_price}  />
+              </SwiperSlide>
+          ))
+        }
+       
+      
+
+    </Swiper>
+    <button className="view-collect">View collection</button>
+      </div>
+ 
+    </div>
+
+  )
+}
+
+export default Ceramiccards
